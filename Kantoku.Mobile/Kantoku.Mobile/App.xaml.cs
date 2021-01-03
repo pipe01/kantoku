1b1c252f15e230b73c9289546b9f16b808c03f31
Felipe Martinez <pipernene@gmail.com>
2021-01-03 01:38:58 +0100

    Add mobile projects

---
+++ Kantoku.Mobile/Kantoku.Mobile/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Kantoku.Mobile.Services;
+using Kantoku.Mobile.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Kantoku.Mobile
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Kantoku.Mobile/Kantoku.Mobile/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Kantoku.Mobile.ViewModels;
+using Kantoku.Mobile.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Kantoku.Mobile
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
