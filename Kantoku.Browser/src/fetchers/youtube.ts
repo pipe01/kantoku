3e82f4f317b9e4debbec915b7fbf7b54afd94fcb
Felipe Martinez <pipernene@gmail.com>
2021-01-11 14:55:42 +0100

    More robust youtube ID fetcher

--- Kantoku.Browser/src/fetchers/youtube.ts
+++ Kantoku.Browser/src/fetchers/youtube.ts
@@ -6,17 +6,22 @@ type EmbedData = {
     title: string;
 }
 
+function getVideoId(): string | null {
+    var result = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/gi.exec(location.href);
+    
+    if (result) {
+        return result[1];
+    }
+    return null;
+}
+
 export default <Fetcher>{
     isSupported() {
-        return /youtube\.com\/watch\?v=/.test(location.href);
+        return !!getVideoId();
     },
     async fetchInfo() {
-        var urlParams = new URLSearchParams(location.search);
-        var videoId = urlParams.get("v");
-        
-        console.log(videoId);
-        
-        
+        const videoId = getVideoId();
+
         var resp = await axios.get<EmbedData>(`https://www.youtube.com/oembed?format=json&url=https://www.youtube.com/watch?v=${videoId}`);
 
         return {
